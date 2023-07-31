59ed616c0575c1fe5176f4a80797556418e3e339
dell du <18588220928@163>
2023-07-31 08:01:06 +0800

    Update

--- project/main.cpp
+++ project/main.cpp
@@ -7,41 +7,8 @@
 ************************************************************************************/
 
 #include <iostream>
-#include <cstring>
 
 #include "clip_text.h"
-
-// head -n 48896 bpe_simple_vocab_16e6.txt | grep -v "bpe_simple_vocab_16e6" > /tmp/bpe_vocab.txt
-// xxd -i /tmp/bpe_vocab.txt bpe_vocab.h
-// unsigned char _tmp_bpe_vocab_txt, _tmp_bpe_vocab_txt_len=524605
-
-// #define BPE_VOCAB_SIZE 48895
-
-// #include "bpe_vocab.h"
-
-// void test()
-// {
-
-// 	uint32_t n = 0;
-// 	char *v[BPE_VOCAB_SIZE], *s, *p;
-
-// 	v[n++] = s = p = (char *) _tmp_bpe_vocab_txt;
-// 	for (; *p && n < BPE_VOCAB_SIZE; ++p) {
-// 		if (*p == '\n') {
-// 			*p = '\0';
-// 			v[n++] = s;
-// 			s = p + 1;
-// 		}
-// 	}
-// 	for (uint32_t i = 0; i < BPE_VOCAB_SIZE; i++) {
-// 		p = strchr(v[i], ' ');
-// 		if (p)
-// 			*p = '\0';
-
-// 		std::cout << i << ": " << v[i] << std::endl;
-// 	}
-// }
-
 
 int main(int argc, char** argv)
 {
